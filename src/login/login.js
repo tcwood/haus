6e562e82f5b148253b377bec4d25998bd9d09362
Travis Wood <travis_cw@yahoo.com>
2018-06-25 14:07:02 -0700

    Add basic log in page elements

--- src/login/login.js
+++ src/login/login.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-const Login = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
-);
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  withoutLabel: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  textField: {
+    flexBasis: 200,
+  },
+});
 
-export default Login;
+class Login extends Component {
+  state = {
+    password: '',
+    showPassword: false,
+    userName: '',
+  };
+
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
+  handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  handleClickShowPassword = () => {
+    this.setState({ showPassword: !this.state.showPassword });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <FormControl>
+          <InputLabel>User Name</InputLabel>
+          <Input value={this.state.userName} onChange={this.handleChange('userName')} />
+        </FormControl>
+        <FormControl>
+          <InputLabel>Password</InputLabel>
+          <Input
+            type={this.state.showPassword ? 'text' : 'password'}
+            value={this.state.password}
+            onChange={this.handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                  onMouseDown={this.handleMouseDownPassword}
+                >
+                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button size="large" variant="contained" color="primary">
+          Log in
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Login);
