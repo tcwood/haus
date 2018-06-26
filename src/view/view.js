e45ac12f8ea8512fa4a33abdc1e1a0627dbe61c4
Travis Wood <travis_cw@yahoo.com>
2018-06-26 16:57:55 -0700

    Fetch hello world data from client

--- src/view/view.js
+++ src/view/view.js
@@ -1,45 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import fakeData from './sampleData.json';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
-  card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    marginBottom: 16,
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-};
+const styles = {};
 
-const View = () => (
-  <div>
-    <h1>View</h1>
-    {fakeData.map(({ date, feedback, id, userName }) => {
-      return (
-        <Card key={id}>
-          <CardContent>
-            <Typography color="textSecondary">{userName}</Typography>
-            <Typography color="textSecondary" paragraph={true}>
-              {feedback}
-            </Typography>
-            <Typography color="textSecondary">{date}</Typography>
-          </CardContent>
-        </Card>
-      );
-    })}
-  </div>
-);
+class View extends Component {
+  state = {
+    data: '',
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log('Error!', err));
+  }
 
+  callApi = async () => {
+    const response = await fetch('/api/home');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error('Yikess! something went wrong>>>', body.message);
+    }
+
+    return body;
+  };
+  render() {
+    return (
+      <div>
+        <h1>View</h1>
+        <p>{this.state.data}</p>
+        {fakeData.map(({ date, feedback, id, userName }) => {
+          return (
+            <Card key={id}>
+              <CardContent>
+                <Typography color="textSecondary">{userName}</Typography>
+                <Typography color="textSecondary" paragraph={true}>
+                  {feedback}
+                </Typography>
+                <Typography color="textSecondary">{date}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
+}
 export default withStyles(styles)(View);
