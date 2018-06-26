1788bfc29ad8128a0e3542b348b1b7920b66e9dc
Travis Wood <travis_cw@yahoo.com>
2018-06-26 13:12:23 -0700

    Add basic create feedback elements

--- src/create/create.js
+++ src/create/create.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Create = () => (
   <div>
     <h1>Create</h1>
+    <TextField
+      id="feedback"
+      label="Feedback"
+      placeholder="Enter your feedback here"
+      multiline
+      margin="normal"
+    />
+    <Button variant="contained" color="primary">
+      Submit Feedback
+    </Button>
   </div>
 );
 
