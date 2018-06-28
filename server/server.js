0113c2eda85b2f9c90f73151dedc798316855b67
Travis Wood <travis_cw@yahoo.com>
2018-06-27 23:20:09 -0700

    Get app ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const getFeedback = require('./handlers/getFeedback');
+const path = require('path');
 const postLogin = require('./handlers/postLogin');
 const postSignup = require('./handlers/postSignup');
 const postCreate = require('./handlers/postCreate');
@@ -17,5 +18,12 @@ app.get('/api/feedback', (req, res) => getFeedback(req, res, feedback));
 app.post('/api/login', (req, res) => postLogin(req, res, users));
 app.post('/api/signup', (req, res) => postSignup(req, res, users));
 app.post('/api/create', (req, res) => postCreate(req, res, feedback));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'build')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
